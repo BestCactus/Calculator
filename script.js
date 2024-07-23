b7b7cc244166734527f1f5f9dde0b4b4f8bc1b39
BestCactus <vojtovidlo.f@gmail.com>
2024-07-23 19:03:15 +0200

    new btns, styles

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//TODO: Make history with localStorage with button for deleting
+
 const numberBtn = document.querySelectorAll(".number-btn");
 const output = document.querySelector("#result");
 
@@ -11,8 +13,28 @@ const pushNumberToOutput = (num) => {
   output.textContent = array.join("");
 };
 
+const clearCalculator = () => {
+  output.innerHTML = "";
+  //TODO: remove numbers from memory
+  //TODO: make animations
+};
+
 numberBtn.forEach((btn) => {
   btn.addEventListener("click", () => {
-    pushNumberToOutput(btn.id);
+    pushNumberToOutput(btn.innerHTML);
   });
 });
+
+const cactusBtn = document.querySelector("#cactus-btn");
+const infinityBtn = document.querySelector("#infinity-btn");
+
+cactusBtn.addEventListener("click", () => {
+  pushNumberToOutput(cactusBtn.innerHTML);
+});
+
+infinityBtn.addEventListener("click", () => {
+  pushNumberToOutput(infinityBtn.innerHTML);
+});
+const clearBtn = document.querySelector("#c-btn");
+
+clearBtn.addEventListener("click", clearCalculator);
